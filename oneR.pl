ccbe77d1c916b3f8ba3ea2c0a96cb3e0a9a51531
Aaron Douthat <aaron.douthat@gmail.com>
2014-06-28 22:34:27 -0700

    more 1R stuff added

--- oneR.pl
+++ oneR.pl
@@ -10,11 +10,11 @@ $Script = basename();
 MAIN_BLOCK:
 {
     if (! @ARGV) {
-        print "usage: $Script data-file predicted-column\n";
+        print "usage: $Script data-file class-column\n";
         exit(1)
     }
     my $data_filename = shift @ARGV;
-    my $predicted_column = shift @ARGV;
+    my $class_column = shift @ARGV;
     
 
     open my $fh, "$data_filename"
@@ -30,15 +30,49 @@ MAIN_BLOCK:
         my @columns = split /,/, $line;
         my $row = {};
         push @$data_table, $row;
-        for(my $i=0; $i < scalar(@columns_names); $i++) {
+        for(my $i=0; $i < scalar(@column_names); $i++) {
             my $name = $column_names[$i];
             my $value = $columns[$i];
             $row->{$name} = $value;
         }
     }
-    
     close $fh
         or die "$Script: cannot close '$data_filename': $!";
+    
+
+    my $col_to_att = {};
+    for( my $i=0; $i<@column_names; $i++) {
+        my $column_name = $column_names[$i];
+        foreach my $row (@$data_table) {
+            my $att = $row->{$column_name};
+            my $class = $row->{$class_column};
+            $col_to_att->{$column_name}->{$att}->{'class-counts'}->{$class}++;
+            $col_to_att->{$column_name}->{$att}->{'totals'}++
+        }
+    }
+
+    my $rules = {};
+    foreach my $column_name (@column_names) {
+        foreach my $att (keys %{$col_to_att->{$column_name}}) {
+            my $occurences = 0;
+            foreach my $class (keys %{$col_to_att->{$column_name}->{$att}->{'class-counts'}}) {
+                if ($occurences < $col_to_att->{$column_name}->{$att}->{'class-counts'}->{$class}) {
+                    $occurences = $col_to_att->{$column_name}->{$att}->{'class-counts'}->{$class};
+                    $col_to_att->{$column_name}->{$att}->{'rule'} = $class;
+                }
+            }
+        }
+    }
+    
+    foreach my $column_name (@column_names) {
+        foreach my $att (keys %{$col_to_att->{$column_name}}) {
+            my $rule = $col_to_att->{$column_name}->{$att}->{'rule'};
+            my $total = $col_to_att->{$column_name}->{$att}->{'totals'};
+            my $rule_count = $col_to_att->{$column_name}->{$att}->{'class-counts'}->{$rule};
+            my $abs_error = $total - $rule_count;
+            $col_to_att->{$column_name}->{$att}->{'error-rate'} = $abs_error / $total;;
+        }
+    }
     
 
     exit(0);
